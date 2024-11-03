d6de872c96126e793cb7ec0b954dcd1fc59f2d86
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-11-03 21:15:31 +0300

    Role Names and Picture TagHelper

---
+++ IdentityAppCookie.Web/TagHelpers/UserPictureTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace IdentityAppCookie.Web.TagHelpers
+{
+    public class UserPictureTagHelper : TagHelper
+    {
+        public string? PictureUrl { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+
+            if (String.IsNullOrEmpty(PictureUrl))
+            {
+                output.Attributes.SetAttribute("src", "/userpicture/download.jpg");
+            }
+            else
+            {
+                output.Attributes.SetAttribute("src", $"/userpicture/{PictureUrl}");
+            }
+        }
+    }
+}
---
+++ IdentityAppCookie.Web/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using IdentityAppCookie.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace IdentityAppCookie.Web.TagHelpers
+{
+    public class UserRoleNamesTagHelper(UserManager<UserApp> userManager) : TagHelper
+    {
+        public string UserId { get; set; } = null!;
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var user = await userManager.FindByIdAsync(UserId);
+
+            var userRoles = await userManager.GetRolesAsync(user!);
+
+            var stringBuilder = new StringBuilder();
+
+            userRoles.ToList().ForEach(userRoles =>
+            {
+                stringBuilder.Append(@$"<span class='badge bg-secondary mx-1'>{userRoles.ToLower()}</span>");
+            });
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+        }
+
+
+    }
+}
