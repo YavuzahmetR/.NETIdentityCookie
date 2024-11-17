b196af170178d0f3ac7ac9a561fb260c5c4e3d2a
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-11-17 17:18:14 +0300

    Extension Methods Added

--- IdentityAppCookie.Web/Program.cs
+++ IdentityAppCookie.Web/Program.cs
@@ -5,6 +5,9 @@ using IdentityAppCookie.Web.Requirements;
 using IdentityAppCookie.Repository.Seeds;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using IdentityAppCookie.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,73 +16,32 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContextWithMigrations(builder.Configuration);
 
-
 builder.Services.AddSecurityStampValidatorOptionsExtension();
 
 builder.Services.AddEmailSettings(builder.Configuration);
 
 builder.Services.AddEmailService();
 
+builder.Services.AddSignService();
+
+builder.Services.AddViolenceRequirementService();
+
+builder.Services.AddExchangeExpireRequirementService();
+
 builder.Services.AddMemberService();
 
 builder.Services.AddClaimService();
-
-builder.Services.AddAuthorization(opt =>
-{
-    opt.AddPolicy("IstanbulPolicy", policy =>
-    {
-        // Claim Exists On Database As A Column Type Claim - AspNetUsers
-        policy.RequireClaim("city", "Ýstanbul"); 
-    });
-
-    opt.AddPolicy("ExchangeExpireDatePolicy", policy =>
-    {
-        // Claim Does Not Exists On DataBase A Column Type - Its In AspNetUserClaims
-        policy.AddRequirements(new ExchangeExpireRequirement()); 
-    });
-
-    opt.AddPolicy("ViolencePolicy", policy =>
-    {
-        // Claim Exists On Database As A Column Type Claim - AspNetUsers
-        policy.AddRequirements(new ViolenceRequirement() { ThresholdAge = 18});
-    });
-
-    opt.AddPolicy("OrderReadAndCreate_StockRead_Permission", policy =>
-    {
-        // Claim Exists On Database As A Column Type Role - AspNetRoles
-        policy.RequireClaim("Permission", Permissions.Order.Read);
-        policy.RequireClaim("Permission", Permissions.Order.Create);
-        policy.RequireClaim("Permission", Permissions.Stock.Read);
-    });
-
-    opt.AddPolicy("OrderReadPermission", policy =>
-    {
-        // Claim Exists On Database As A Column Type Role - AspNetRoles
-        policy.RequireClaim("Permission", Permissions.Order.Read);
-    });
-
-    opt.AddPolicy("OrderCreatePermission", policy =>
-    {
-        // Claim Exists On Database As A Column Type Role - AspNetRoles
-        policy.RequireClaim("Permission", Permissions.Order.Create);
-
-    });
-
-    opt.AddPolicy("StockReadPermission", policy =>
-    {
-        // Claim Exists On Database As A Column Type Role - AspNetRoles
-        policy.RequireClaim("Permission", Permissions.Stock.Read);
-    });
-});
-
-builder.Services.AddScoped<IAuthorizationHandler, ExchangeExpireRequirementHandler>();
-builder.Services.AddScoped<IAuthorizationHandler, ViolenceRequirementHandler>();
 
 builder.Services.AddIdentityExtension();
 
 builder.Services.ConfigureApplicationCookieExtension();
 
-builder.Services.AddSingletonFileProvider(); 
+builder.Services.AddSingletonFileProvider();
+
+builder.Services.AddAuthorizationExtension();
+
+builder.Services.AddThirdPartyAuthenticationExtension(builder.Configuration);
+
 
 var app = builder.Build();
 
